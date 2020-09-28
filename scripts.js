a3a018203231b6d2ab2dd4416f77bcc467973745
Victor Hugo <victorhugo.cercasin@gmail.com>
2020-09-28 17:13:14 -0300

    dia 28/09

--- scripts.js
+++ scripts.js
@@ -22,18 +22,13 @@ function popular(membrosJson) {
     let diametro = gerarDiametro();
     let pos = gerarPos(membros, diametro);
     membros[i] = new Membro(id, diametro, pos, membrosJson, membros);
-    
-    
-
   }
   let memoria = []
   for (let i = 0; i < membros.length; i++) {
     criarSvg(membros, i);
     memoria[i] = document.getElementById(membrosJson.membrosLista[i].nome).cloneNode(true);
-    document.getElementById(membrosJson.membrosLista[i].nome).addEventListener("mouseout",function() {mouseOut(this, memoria[i])})
-    document.getElementById(membrosJson.membrosLista[i].nome).addEventListener("mouseover",function(){mouseOver(this)})
-    
-
+    document.getElementById(membrosJson.membrosLista[i].nome).addEventListener("mouseover", function(){mouseOver(this,membrosJson, i)})
+    document.getElementById(membrosJson.membrosLista[i].nome).addEventListener("mouseout", function(){mouseOut(this, memoria[i])})
   }
 }
 class Membro {
@@ -77,7 +72,7 @@ function gerarPos(membros, diametro) {
 function gerarPosAleatorio() {
   pos = {
     x: Math.floor(Math.random() * 60 + 10),
-    y: Math.floor(Math.random() * 75 + 10),
+    y: Math.floor(Math.random() * 75 + 15),
   };
   posCorrigido = gerarRelacaoPixelPorcentagem(pos);
   return posCorrigido;
@@ -108,7 +103,7 @@ function gerarRelacaoPixelPorcentagem() {
   let posYPx = (pos.y * yPx) / 100;
   return { x: posXPx, y: posYPx };
 }
-function mouseOver(img){
+function mouseOver(img, membrosJson, i){
   var clone = img.cloneNode(true);
   img.style.zIndex = 2;
   img.style.height = 200 + "px";
@@ -118,46 +113,60 @@ function mouseOver(img){
   var infoBox = document.createElement("div");
   infoBox.style.display = 'inline';
   infoBox.id = img.id + "Info";
+  infoBox.style.color = "rgba(36, 36, 64, 0.8)";
   infoBox.style.position= "absolute";
   infoBox.style.left = img.style.left ;
   infoBox.style.top = img.style.top ;
   infoBox.style.marginTop = "-100px";
   infoBox.style.marginLeft = "0px";
   infoBox.style.height = "200px";
-  infoBox.style.width = "200px";
-  infoBox.style.backgroundColor = "black";
+  infoBox.style.width = "300px";
+  infoBox.style.backgroundColor = "#004080";
   infoBox.style.visibility = "hidden";
   infoBox.style.transitionDelay = "0.3s";
   document.getElementById("corpo").appendChild(infoBox);
-  img.addEventListener("mousemove",setTimeout(function(){mostrarInfoBox(infoBox)}, 200))
-  img.addEventListener("mouseout", function(){apagarInfoBox(infoBox)});
-  function mostrarInfoBox(infoBox){
-    infoBox.style.visibility = "visible";
-    infoBox.style.display = 'inline';
-    infoBox.id = img.id + "Info";
-    infoBox.style.position= "absolute";
-    infoBox.style.left = img.style.left ;
-    infoBox.style.top = img.style.top ;
-    infoBox.style.marginTop = "-100px";
-    infoBox.style.marginLeft = "0px";
-    infoBox.style.height = "200px";
-    infoBox.style.width = "400px";
-    infoBox.style.zIndex = "0"
-    infoBox.style.backgroundColor = "black";
-    infoBox.style.transitionDuration = "0.3s";
+  img.addEventListener("mousemove", () => {setTimeout(() => {mostrarInfoBox(infoBox, membrosJson, i);}, 10)});
+  img.addEventListener("mouseout", () => {apagarInfoBox(infoBox)});
+  function mostrarInfoBox(infoBox, membrosJson, i){
+    if(!infoBox.style.borderRadius){
+      infoBox.style.borderRadius= "5px";
+      infoBox.style.border = "thin solid CornflowerBlue";
+      infoBox.style.visibility = "visible";
+      infoBox.style.display = 'inline';
+      infoBox.id = img.id + "Info";
+      infoBox.style.position= "absolute";
+      infoBox.style.left = img.style.left ;
+      infoBox.style.top = img.style.top ;
+      infoBox.style.marginTop = "-100px";
+      infoBox.style.marginLeft = "0px";
+      infoBox.style.height = "200px";
+      infoBox.style.width = "400px";
+      infoBox.style.zIndex = "1"
+      infoBox.style.backgroundColor = "rgba(36, 36, 64, 0.8)";
+      infoBox.style.transitionDuration = "0.3s";
+      infoBox.style.fontFamily = "Geneva";
+      infoBox.style.fontSize = "20px";
+      infoBox.style.color = "rgba(255, 255,255,0.6)";
+      let infoBoxUl = document.createElement("ul");
+      infoBoxUl.innerHTML = "<h4>"+membrosJson.membrosLista[i].nome+"</h4>"+
+                            "<li>"+membrosJson.membrosLista[i].cargo+"</li>"+
+                            "<li>"+membrosJson.membrosLista[i].email+"</li>"+
+                            "<li>"+membrosJson.membrosLista[i].github+"</li>";
+      infoBoxUl.style.marginLeft = "20%";
+      infoBoxUl.style.listStyle = "none";
+      infoBox.appendChild(infoBoxUl);
+    }
+    else{};
   }
   function apagarInfoBox(infoBox){
-    infoBox.style.visibility = "hidden";
+    infoBox.remove();
   }
-
 }
 function mouseOut(img, memoria){
   img.style.height = memoria.style.height;
   img.style.width = memoria.style.width;
   img.style.margin = memoria.style.margin;
   img.style.zIndex = memoria.style.zIndex;
-
-
 }
 
 
